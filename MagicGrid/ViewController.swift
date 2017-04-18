ec4f9afa3c76551aa8a4f32a3de321a2d9f20235
Victor Lee <myleevictor@gmail.com>
2017-04-18 11:10:32 +0800

    initial for loop to populate random colors

--- MagicGrid/ViewController.swift
+++ MagicGrid/ViewController.swift
@@ -12,12 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 0...5 {
+            let redView = UIView()
+            redView.backgroundColor = randomColor()
+            redView.frame = CGRect(x: i * 100, y: 0, width: 100, height: 100)
+            view.addSubview(redView)
+            
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    fileprivate func randomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
 
 
