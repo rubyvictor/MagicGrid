cae7eede686994d8e0ac7a25ec0bf2dc100a6ffb
Victor Lee <myleevictor@gmail.com>
2017-04-18 14:08:37 +0800

    created grid using 2 for loops for x and y, and added UIPanGestureRecognizer for xy point location

--- MagicGrid/ViewController.swift
+++ MagicGrid/ViewController.swift
@@ -13,13 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for i in 0...5 {
-            let redView = UIView()
-            redView.backgroundColor = randomColor()
-            redView.frame = CGRect(x: i * 100, y: 0, width: 100, height: 100)
-            view.addSubview(redView)
-            
+        let numViewPerRow = 15
+        let width = view.frame.width / CGFloat(numViewPerRow)
+        
+        
+        for j in 0...30 {
+            for i in 0...numViewPerRow {
+                let cellView = UIView()
+                cellView.backgroundColor = randomColor()
+                cellView.frame = CGRect(x: CGFloat(i) * width, y: CGFloat(j) * width, width: width, height: width)
+                cellView.layer.borderWidth = 0.5
+                cellView.layer.borderColor = UIColor.black.cgColor
+                view.addSubview(cellView)
+            }
         }
+        
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
+        
+        
+    }
+    
+    func handlePan(gesture: UIPanGestureRecognizer) {
+        let location = gesture.location(in: view)
+        print(location)
         
         
     }
